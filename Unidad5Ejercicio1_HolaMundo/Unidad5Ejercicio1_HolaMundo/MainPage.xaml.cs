9fa2c2b4f35f8859e0b5e7d55dec256d2b040c69
JuanMGallego <jmgallego@iesnervion.es>
2023-10-05 14:30:25 +0200

    Jueves Interfaces y Gestion

---
+++ Unidad5Ejercicio1_HolaMundo/Unidad5Ejercicio1_HolaMundo/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Unidad5Ejercicio1_HolaMundo
+
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            string result = await DisplayPromptAsync("Pregunta", "¿Cómo te apellidas?");
+            await DisplayAlert("Saludo", "Hola " + nombre.Text + " " + result, "OK");
+        }
+    }
+}
