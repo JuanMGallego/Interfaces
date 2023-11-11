ca1451367763138933ce624c9ca1c3a02508cd14
JuanMGallego <jmgallego@iesnervion.es>
2023-11-11 23:22:55 +0100

    fsd

--- PlacasSolares/PlacasSolares/Views/Gestion.xaml.cs
+++ PlacasSolares/PlacasSolares/Views/Gestion.xaml.cs
@@ -5,5 +5,13 @@ public partial class Gestion : ContentPage
 	public Gestion()
 	{
 		InitializeComponent();
-	}
+
+		BindingContext = this;
+        DateTime fecha = DateTime.Now;
+        string mes = fecha.ToString("MMM");
+        mes = char.ToUpper(mes[0]) + mes.Substring(1);
+        Fecha.Text = fecha.ToString("dd ") + mes + fecha.ToString(" yyyy");
+
+    }
+
 }
