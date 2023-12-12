a47999cc8ac63c6896bb931ee0fdc7f99e68967f
JuanMGallego <jmgallego@iesnervion.es>
2023-12-12 14:38:52 +0100

    Unidad11 Empezada

---
+++ Unidad11_Ejercicios/Unidad11_Ejercicios/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Unidad11_Ejercicios
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Unidad11_Ejercicios/Unidad11_Ejercicios/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Unidad11_Ejercicios
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
