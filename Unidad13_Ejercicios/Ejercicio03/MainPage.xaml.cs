1290a890de5dc500632646c23ee3302ad44c56ea
JuanMGallego <jmgallego@iesnervion.es>
2024-01-12 11:12:33 +0100

    Ejecicios 3 y 4 terminados

---
+++ Unidad13_Ejercicios/Ejercicio03/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio03
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Unidad13_Ejercicios/Ejercicio04/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio04
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
