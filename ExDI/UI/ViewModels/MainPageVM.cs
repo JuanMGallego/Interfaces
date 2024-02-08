bac70d763dbcadc131aba010f98c6b61c5828107
JuanMGallego <jmgallego@iesnervion.es>
2024-02-08 14:39:16 +0100

    dddvgsds

---
+++ ExDI/UI/ViewModels/MainPageVM.cs
@@ -0,0 +1,46 @@
+﻿using Ejercicio03CRUD.ViewModels.Utilis;
+using System.Collections.ObjectModel;
+using UI.DAL;
+using UI.Models;
+
+namespace UI.ViewModels
+{
+    class MainPageVM : clsVMBase
+    {
+        private ObservableCollection<clsPersonaModel> _listadoPersonas;
+
+        public MainPageVM()
+        {
+            ListadoPersonas = new ObservableCollection<clsPersonaModel>();
+            _ = CargarListadoPersonas();
+        }
+
+        public ObservableCollection<clsPersonaModel> ListadoPersonas
+        {
+            get { return _listadoPersonas; }
+            set
+            {
+                _listadoPersonas = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private async Task CargarListadoPersonas()
+        {
+            var personasDAL = new clsListadoPersonasDAL();
+            var listaPersonas = await personasDAL.getPersonasDAL();
+
+            foreach (var persona in listaPersonas)
+            {
+                // Mapear clsPersona a clsPersonaModel
+                var personaModel = new clsPersonaModel
+                {
+                    Nombre = persona.Nombre,
+                    Apellidos = persona.Apellidos
+                };
+
+                ListadoPersonas.Add(personaModel);
+            }
+        }
+    }
+}
